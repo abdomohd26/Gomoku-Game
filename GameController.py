e92e9f1ab371c1ad38416cf7c44894cc5f76e0de
Abdelrahman Mohamed <boodymohammed26@gmail.com>
2025-05-11 11:35:38 +0300

    change background color for win and lose statement

--- GameController.py
+++ GameController.py
@@ -10,7 +10,7 @@ from Algorithm import AlgorithmAlphaBeta, AlgorithmMinimax
 class Graphical:
     def __init__(self):
         pygame.init()
-        self.length = 15
+        self.length = 6
         self.b = Board(self.length)
         self._screen = pygame.display.set_mode((45 * self.length, 45 * self.length))
         self._service = Service(self.b)
@@ -63,7 +63,7 @@ class Graphical:
         pygame.display.update()
 
         if mode == "human_vs_ai":
-            alg = AlgorithmMinimax(3)
+            alg = AlgorithmMinimax(1)
             self._service = Service1(self.b, alg)
             game_over = False
 
@@ -92,15 +92,21 @@ class Graphical:
             font = pygame.font.Font('freesansbold.ttf', 70)
 
             if self._service.get_turn() == -1:
-                text = font.render('You Won!', True, (0, 0, 143))
+                text = font.render('You Won!', True, (255, 255, 255))
                 text_rect = text.get_rect()
                 text_rect.center = (45 * self.length // 2, 45 * self.length // 2)
+                pygame.draw.rect(self._screen, (0, 128, 0), text_rect.inflate(20, 10))
+
                 self._screen.blit(text, text_rect)
+
             elif self._service.get_turn() == 1:
-                text = font.render('You Lost!', True, (0, 0, 150))
+                text = font.render('You Lost!', True, (255, 255, 255))
                 text_rect = text.get_rect()
                 text_rect.center = (45 * self.length // 2, 45 * self.length // 2)
+                pygame.draw.rect(self._screen, (128, 0, 0), text_rect.inflate(20, 10))
+
                 self._screen.blit(text, text_rect)
+                
 
             pygame.display.update()
 
@@ -111,8 +117,8 @@ class Graphical:
                         r = False
             pygame.quit()
         else:
-            alg1 = AlgorithmAlphaBeta(5)
-            alg2 = AlgorithmMinimax(3)
+            alg1 = AlgorithmMinimax(3)
+            alg2 = AlgorithmAlphaBeta(3)
             self._service = Service2(self.b, alg1, alg2)
 
             while True:
@@ -129,18 +135,26 @@ class Graphical:
                 time.sleep(0.5)
                 pygame.display.update()
 
+                if Utils.game_over(self._service.get_board(), -1):
+                    break
+
             self.draw_board()
-            font = pygame.font.Font('freesansbold.ttf', 70)
+            font = pygame.font.Font('freesansbold.ttf', 50)
 
             if self._service.get_turn() == -1:
-                text = font.render('Algorithm 1 Wins!', True, (0, 0, 143))
+                text = font.render('Minimax Algorithm Won!', True, (255, 255, 255))
                 text_rect = text.get_rect()
                 text_rect.center = (45 * self.length // 2, 45 * self.length // 2)
+                pygame.draw.rect(self._screen, (54, 116, 181), text_rect.inflate(20, 10))
+
                 self._screen.blit(text, text_rect)
+
             elif self._service.get_turn() == 1:
-                text = font.render('Algorithm 2 Wins!', True, (0, 0, 150))
+                text = font.render('Alpa Beta Algorithm Won!', True, (255, 255, 255))
                 text_rect = text.get_rect()
                 text_rect.center = (45 * self.length // 2, 45 * self.length // 2)
+                pygame.draw.rect(self._screen, (54, 116, 181), text_rect.inflate(20, 10))
+
                 self._screen.blit(text, text_rect)
 
             pygame.display.update()
